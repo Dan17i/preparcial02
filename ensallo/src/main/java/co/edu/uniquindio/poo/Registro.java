b37772cf7aa0722d159a194ea8ec1c93a941bfe8
Daniel Jurado <daniele.juradoc@uqvirtual.edu.co>
2023-09-15 00:15:35 -0500

    espacios

--- ensallo/src/main/java/co/edu/uniquindio/poo/Registro.java
+++ ensallo/src/main/java/co/edu/uniquindio/poo/Registro.java
@@ -3,22 +3,19 @@ package co.edu.uniquindio.poo;
 import java.util.ArrayList;
 
 public class Registro {
-
+ // creo el Arraylist 
     ArrayList<Mueble> listMuebles;
-
+ // instacio mueble
     Mueble mueble = new Mueble((float)2000, "Gjdnsd", (byte)23, Madera.ALMENDRO, Tipo.ALCOBA); 
-
-    
+// agrego un muebke a la Arraylist
     public void agragarMueble (Mueble muebles){
         listMuebles.add(muebles);
     }
-    
-
-
+    //  remuevo un mueble de la lista si esta, si no no pasa nada
     public void retiara(Mueble mueble) {
      listMuebles.remove(mueble); 
     }
-
+ // consulto que este el elemento
    public Boolean consulta(Mueble mueble){
     boolean existe =listMuebles.contains(mueble);
     return existe;
